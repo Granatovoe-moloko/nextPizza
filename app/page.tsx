8d474ee6487150afd280dbb90b10442a9fd9d1c3
Анастасия <>
2025-08-20 12:36:30 +0300

    убрана лишняя папка nextpizza + подключение шапки и заголовка

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import { Container, Title } from "@/components/shared";
+
+export default function Home() {
+  return <>
+    <Container className="mt-10">
+      <Title text="Все пиццы" size="xl" className="font-bold"/>
+    </Container>
+  </>;
+}
